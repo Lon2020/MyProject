46b135f562fd1cef4fa7989c03350b2d793ed0e3
Lon2020 <65143732+Lon2020@users.noreply.github.com>
2021-11-15 15:58:09 +1100

    Add files via upload

---
+++ Banking System C# Console application/Assignment 1 13754306/Model/Account.cs
@@ -0,0 +1,209 @@
+﻿using Assignment_1_13754306.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1_13754306.Model
+{
+    //Model for Account
+    class Account
+    {
+        // Data types and variable
+        private int accountID;
+        private string customerName;
+        private double accountBal = 0; // every new account created, the account balance will always be $0
+
+        //Constructor requires account id and customer name used in createAccount view
+        public Account(int accountID, string customerName)
+        {
+            this.accountID = accountID;
+            this.customerName = customerName;
+        }
+
+        //Constructor for just calling Account model
+        public Account()
+        {
+        }
+
+        //get account id
+        public int getAccountID()
+        {
+            return accountID;
+        }
+
+        //set account id with new value
+        public void setAccountID(int accountID)
+        {
+            this.accountID = accountID;
+        }
+
+        //get customer name
+        public string getCustomerName()
+        {
+            return customerName;
+        }
+
+        //set customer name with new value
+        public void setCustomerName(string customerName)
+        {
+            this.customerName = customerName;
+        }
+
+        //get account balance used in deposit and withdraw views
+        public double getAccountBal()
+        {
+            return accountBal;
+        }
+
+        //set account balance with new value used in deposit and withdraw views
+        public void setCustomerNameAccountBal(double accountBal)
+        {
+            this.accountBal = accountBal;
+        }
+
+
+        //Data Validation for Account Id
+        public int checkAccountId()
+        {
+            string accountId;
+            bool checkAccountIdLength = true;
+            bool checkAccountInteger = true;
+            do
+            {
+                // set cursor position and read the user input for account id
+                Console.SetCursorPosition(40, 6);
+                accountId = Console.ReadLine();
+
+                //check if account id is not empty and less than 10 digits
+                checkAccountIdLength = accountId.ToString().Length != 0 && accountId.ToString().Length < 10;
+                //check if account id contains only number
+                checkAccountInteger = accountId.All(char.IsDigit);
+
+                //if account id contains any letter
+                if (!checkAccountInteger)
+                {
+                    viewBorder.cursor("Please enter only number", 55, 6); // display error
+                }
+                else if (!checkAccountIdLength) // if account id is empty and more than 10 digits
+                {
+                    viewBorder.cursor("The number should be less than 10 digits", 55, 6);//display error
+                }
+
+            } while (!checkAccountIdLength || !checkAccountInteger); // break the do while loop when account id is validated
+
+            // return account id in Int32
+            return Convert.ToInt32(accountId);
+        }
+
+
+        //Data Validation for Deposit Amount
+        public double checkDepositAmount()
+        {
+            double depositAmount = 0;
+            bool checkDeposit = true;
+            do
+            {
+                Console.SetCursorPosition(42, 8);
+
+                try
+                {
+                    // read deposit amount 
+                    depositAmount = Convert.ToDouble(Console.ReadLine());
+
+                    // if deposit amount is negative number
+                    if (depositAmount < 0)
+                    {
+                        viewBorder.cursor("Please enter only positive number", 25, 9); // display error
+                        checkDeposit = true;  // checkDeposit = true and stay in the loop;
+                    }
+                    else // if deposit amount is positive amount
+                    {
+                        // break the loop
+                        checkDeposit = false; 
+                    }
+                }
+                catch (FormatException e)//catch error if deposit amount input contains string
+                {
+                    viewBorder.cursor("Please enter only number", 55, 8); // display error
+                    checkDeposit = true; // checkDeposit = true and stay in the loop;
+                }
+            } while (checkDeposit);
+
+            // return deposit amount
+            return depositAmount;
+        }
+
+        //Data Validation for Withdrawal Amount
+        public double checkWithdrawAmount()
+        {
+            double withdrawAmount = 0;
+            bool checkWithdraw = true;
+            do
+            {
+                Console.SetCursorPosition(44, 8);
+                try
+                {
+                    // read withdraw amount 
+                    withdrawAmount = Convert.ToDouble(Console.ReadLine());
+
+                    // if withdraw amount is negative number
+                    if (withdrawAmount < 0)
+                    {
+                        viewBorder.cursor("Please enter only positive number", 25, 9); // display error
+                        checkWithdraw = true; // checkWithdraw = true and stay in the loop;
+                    }
+                    else // if withdraw amount is positive amount
+                    {
+                        // break the loop
+                        checkWithdraw = false; 
+                    }
+                }
+                catch (FormatException e)//catch error if withdraw amount input contains string
+                {
+                    viewBorder.cursor("Please enter only number", 55, 8); // display error
+                    checkWithdraw = true; // checkWithdraw = true and stay in the loop;
+                }
+            } while (checkWithdraw);
+
+            // return withdraw amount
+            return withdrawAmount;
+        }
+
+        public bool checkChoice()
+        {
+            string choice;
+            bool checkChoice = true;
+            do
+            {
+                // display when user entered account id and file is not found
+                viewBorder.cursor("Account not found, Check another account (y/n):", 25, 7);
+                Console.SetCursorPosition(73, 7);
+                choice = Console.ReadLine();
+
+                //remove white space from choice e.g. "y " to "y" with no spaces
+                string choiceTrim = choice.Trim();
+                if (choiceTrim.Equals("y") || choiceTrim.Equals("Y"))// if user enter y or Y
+                {
+                    checkChoice = false; // break the loop
+                }
+                else if (choiceTrim.Equals("n") || choiceTrim.Equals("N"))// if user enter n or N
+                {
+                    //go back to main menu and break the loop
+                    mainMenuView.mainMenu(); 
+                    checkChoice = false;
+                }
+                else
+                {
+                    //break the loop
+                    checkChoice = true;
+                }
+            } while (checkChoice);
+
+            // return true
+            return true;
+        }
+    }
+}
+
---
+++ Banking System C# Console application/Assignment 1 13754306/Model/Customer.cs
@@ -0,0 +1,158 @@
+﻿using Assignment_1_13754306.View;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Assignment_1_13754306.Model
+{
+    //Model for Customer
+    public class Customer
+    {
+        // Data types and variable for customer
+        private string firstName;
+        private string lastName;
+        private string address;
+        private long phone;
+        private string email;
+
+        //Constructor
+        public Customer(string firstName, string lastName, string address, long phone, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.phone = phone;
+            this.email = email;
+        }
+
+        //Constructor for just calling Customer model
+        public Customer()
+        {
+        }
+
+        //get customer first name
+        public string getFirstName()
+        {
+            return firstName;
+        }
+
+        //set first name with a new value
+        public void setFiestName(string firstName)
+        {
+            this.firstName = firstName;
+        }
+
+        // get customer last name
+        public string getLastName()
+        {
+            return lastName;
+        }
+
+        // set customer last name with a new value
+        public void setLastName(string lastName)
+        {
+            this.lastName = lastName;
+        }
+
+        // get customer address
+        public string getAddress()
+        {
+            return address;
+        }
+
+        // set customer address with new address
+        public void setAddress(string address)
+        {
+            this.address = address;
+        }
+
+        // get customer phone 
+        public long getPhone()
+        {
+            return phone;
+        }
+
+        //set customer phone with a new phone
+        public void setPhone(long phone)
+        {
+            this.phone = phone;
+        }
+
+        // get customer email
+        public string getEmail()
+        {
+            return email;
+        }
+
+        // set customer email with a new email
+        public void setEmail(string email)
+        {
+            this.email = email;
+        }
+
+
+        //method for searching all acounts and return in string array
+        public string[] searchAccountCustomer(int accountId)
+        {
+            string[] files = new string[12];
+            try
+            {
+                DirectoryInfo filePath = new DirectoryInfo(@"./");//Retrieve all files' name inside the Debug folder
+                FileInfo[] Files = filePath.GetFiles("*.txt");//Retrieve all files' name inside the Debug folder
+                bool fileExist = false;
+                string accountIdString = "";
+
+                //reformat account id to accountid.txt
+                accountIdString = accountId.ToString() + ".txt";
+
+                //loop for all existing files
+                foreach (FileInfo file in Files)
+                {
+                    //boolean to check if account id equals to existing file
+                    fileExist = accountIdString.Equals(file.Name);
+                    if (fileExist == true)
+                    {
+                        // if account id exists, break a for loop for a better performance, so that it does not to loop until the last file
+                        break;
+                    }
+                }
+
+                // if account exists
+                if (fileExist)
+                {
+                    //display found!
+                    viewBorder.cursor("Account Found!!!", 25, 11);
+                    try
+                    {
+                        //read all lines for that file and stored in files array declared above
+                        files = System.IO.File.ReadAllLines($@"{accountIdString}");
+                        
+                    }
+                    catch (FileNotFoundException e)//catch if file not fould
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+                }
+                else//if account is not exist
+                {
+                    // set files index 0 to be empty to be check in views
+                    files[0] = "empty";
+                }
+            }
+            catch (FileNotFoundException e) // catch the file not found
+            {
+                // display error
+                Console.WriteLine(e.Message);
+            }
+
+            // return files
+            return files;
+
+        }
+    }
+}
